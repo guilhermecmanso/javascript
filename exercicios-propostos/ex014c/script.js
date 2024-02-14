6f6b4a920af645cd17a0edcd2f6be92eb3168e2b
Guilherme Manso <guilherme1manso@gmail.com>
2024-02-14 19:47:40 -0300

    revisão ex014 JavaScript

---
+++ exercicios-propostos/ex014c/script.js
@@ -0,0 +1,22 @@
+
+function carregar() {
+    var date = new Date()
+    var hora = 13
+    var msg = document.getElementById('msg')
+    var img = document.getElementById('imagem')
+    
+
+    if (hora >=0 && hora < 12) {
+        img.src = 'imagens/manha-png.png'
+        document.body.style.backgroundColor = '#C89138'
+        msg.innerHTML = `Agora são <strong>${hora} horas</strong> da manhã.`
+    } else if (hora > 12 && hora < 18 ){
+        img.src = 'imagens/tarde-png.png'
+        document.body.style.backgroundColor = '#DF8B3A'
+        msg.innerHTML = `Agora são <strong>${hora} horas</strong> da tarde.`
+    } else {
+        img.src = 'imagens/noite-png.png'
+        document.body.style.backgroundColor = '#0C1719'
+        msg.innerHTML = `Agora são <strong>${hora} horas</strong> da noite.`
+    }
+}
