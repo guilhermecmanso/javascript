b0d8cc3fa08c989b5b95a8e16957317f242b496d
Guilherme Manso <guilherme1manso@gmail.com>
2024-02-14 21:50:19 -0300

    att

--- exercicios-propostos/ex014c/script.js
+++ exercicios-propostos/ex014c/script.js
@@ -2,6 +2,7 @@
 function carregar() {
     var date = new Date()
     var hora = date.getHours()
+    var min = date.getMinutes()
     var msg = document.getElementById('msg')
     var img = document.getElementById('imagem')
     
@@ -9,16 +10,16 @@ function carregar() {
     if (hora >=0 && hora < 12) {
         img.src = 'imagens/manha-png.png'
         document.body.style.backgroundColor = '#C89138'
-        msg.innerHTML = `Agora são <strong>${hora} horas</strong> da manhã.`
+        msg.innerHTML = `Agora são <strong>${hora}h</strong> e <strong>${min}min</strong>.`
 
     } else if (hora > 12 && hora < 18 ){
         img.src = 'imagens/tarde-png.png'
         document.body.style.backgroundColor = '#DF8B3A'
-        msg.innerHTML = `Agora são <strong>${hora} horas</strong> da tarde.`
+        msg.innerHTML = `Agora são <strong>${hora}h</strong> e <strong>${min}min</strong>.`
 
     } else {
         img.src = 'imagens/noite-png.png'
         document.body.style.backgroundColor = '#071C1F'
-        msg.innerHTML = `Agora são <strong>${hora} horas</strong> da noite.`
+        msg.innerHTML = `Agora são <strong>${hora}h</strong> e <strong>${min}min</strong>.`
     }
 }
