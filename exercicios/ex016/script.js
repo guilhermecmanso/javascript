69df6ee257460f831556f314507704c232c8bce9
Guilherme Manso <guilherme1manso@gmail.com>
2024-02-18 17:44:39 -0300

    att

---
+++ exercicios/ex016/script.js
@@ -0,0 +1,24 @@
+var inicio = 1
+var fim = 10
+var passo = 1
+var cont = ''
+
+for (var cont = inicio; cont <= fim ; cont + passo) {
+    console.log(`Passo ${cont}`)
+}
+
+// function contar() {
+//     var inicio = document.querySelector ('input#inicio')
+//     var fim = document.querySelector('input#fim')
+//     var passo = document.querySelector('input#passo')
+//     var result = document.querySelector('div#result')
+    
+//     if (inicio.value.length <= 0) {
+//         result.innerHTML = 'Não foi possível contar! <br>Informe um número de início.'
+//     } else {
+        
+        
+//     }
+    
+    
+// }
