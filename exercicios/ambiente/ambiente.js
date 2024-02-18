e73ac110d8368ae10359c63e7a45c8f53fbe2820
Guilherme Manso <guilherme1manso@gmail.com>
2024-02-18 16:07:07 -0300

    att

--- exercicios/ambiente/ambiente.js
+++ exercicios/ambiente/ambiente.js
@@ -1,13 +1,22 @@
-var c = 1
 
-do {
+console.log('Começando...')
+for (var c=1; c <=5; c++) {
     console.log(`Passo ${c}`)
-    c++
-} while (c <= 40)
+}
+console.log('Acabou!')
 
 
 
 /*
+var c = 1
+do {
+    console.log(`Passo ${c}`)
+    c++
+} while (c <= 80)
+
+
+
+
 while (c <= 70) {
     console.log(`Passo ${c}`)
     c++
