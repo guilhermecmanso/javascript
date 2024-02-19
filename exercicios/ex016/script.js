44b5391529c5bd804e540ea53891512acbb67240
Guilherme Manso <guilherme1manso@gmail.com>
2024-02-18 21:03:44 -0300

    att

--- exercicios/ex016/script.js
+++ exercicios/ex016/script.js
@@ -5,26 +5,30 @@ function contar() {
     var result = document.querySelector('div#result')
 
     
-    if (ini.value.length == 0 || fim.value.length == 0 || passo.value.length == 0) {
-        alert('Não é possível calcular! \nFaltam dados!')
+    if (ini.value.length == 0 || fim.value.length == 0) {
+        result.innerHTML = 'Não foi possível calcular!'
+        alert('Dados incompletos!')
     } else { 
-        result.innerHTML = 'Contando: '
+        result.innerHTML = 'Contando: <br>'
         let i = Number(ini.value)
         let f = Number(fim.value)
         let p = Number(passo.value)
-
+        if (p <= 0) {
+            alert('Passo inválido! Considerando PASSO 1')
+            p = 1
+        }
         if (i < f) {
             for (let c = i; c <= f; c += p) {
             result.innerHTML += `${c} \u{1F449}`
-        }
+        } result.innerHTML += '🏴'
         
         } else {
             for (let c = i; c >= f; c -= p) {
             result.innerHTML += `${c} \u{1F449}`
-        } 
+        } result.innerHTML += '🏴'
         
         } 
-    } result.innerHTML += '🏴'
+    } 
         
 
 }
